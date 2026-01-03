da701592f621e112d3ca9f36663b57fd30432144
Nicolas Martins <nicolasmartins@Mac-mini-de-T2.local>
2026-01-03 11:11:42 -0300

    refactor: troca MySQL para Postgres

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "rds" {
   identifier              = "${var.environment}-rds-${var.db_name}"
-  engine                  = "mysql"
-  engine_version          = "8.0"
+  engine                  = "postgres"
+  engine_version          = "16.3"
   instance_class          = var.db_instance_class
   db_name                 = var.db_name
   username                = var.db_username
@@ -10,10 +10,13 @@ resource "aws_db_instance" "rds" {
   storage_type            = "gp3"
   db_subnet_group_name    = aws_db_subnet_group.rds_subnets.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  publicly_accessible     = false
+  apply_immediately       = true
+  storage_encrypted       = true
 
   skip_final_snapshot     = true
   deletion_protection     = false
   multi_az                = var.multi_az
   backup_retention_period = 7
-  port                    = 3306
+  port                    = 5432
 }
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,11 +1,11 @@
 resource "aws_security_group" "rds_sg" {
   name        = "${var.environment}-rds-sg"
-  description = "Allow MySQL only from EKS nodes"
+  description = "Allow Postgres only from EKS nodes"
   vpc_id      = data.terraform_remote_state.eks.outputs.vpc_id
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
+    from_port       = 5432
+    to_port         = 5432
     protocol        = "tcp"
     security_groups = [data.terraform_remote_state.eks.outputs.eks_node_sg]
   }
